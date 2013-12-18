a9cc2f732e94d2428ad3c80fb3d550eed5125d00
Alex <aw3s0me@rocketmail.com>
2013-12-18 14:04:52 +0400

    added sanitize function

--- sanitize.php
+++ sanitize.php
@@ -1,4 +1,11 @@
 ﻿<?php
-
+	function Sanitize($string)
+	{
+		$string = mysql_real_escape_string($string);
+		if (get_magic_quotes_gpc()) $string = stripslashes($string);
+		$string = htmlentities($string);
+		$string = strip_tags($string);
+		return $string;
+	}
 
 ?>
