c082c19090fd88da6ef4c21c256553ce1a8432fb
Alex <aw3s0me@rocketmail.com>
2013-12-18 13:46:36 +0400

    Added Authhelper and user classes

--- authhelper.php
+++ authhelper.php
@@ -1,9 +1,24 @@
 ﻿<?php
+	include_once 'autoload.php';
 	class AuthHelper
 	{
+		public $user;
+		private $table_name;
+		private $dbhelper;
 		
+		public __construct($username, $password, $table_name, $dbhelper) 
+		{
+			$this->table_name = $table_name;
+			$this->user = new User($username, $password);
+			$this->dbhelper = $dbhelper;
+		}
 	
-	
+		public MysqlAuthenticate(){
+			if (!dbhelper || !user) return;
+			$query = "SELECT * FROM $table_name WHERE username='$user->$username' AND password='$user->$password'";
+			$result = $dbhelper->RunQuery($query);
+			return $result;
+		}
 	
 	}
 	
--- user.php
+++ user.php
@@ -1,9 +1,18 @@
 ﻿<?php
+
+	include_once 'sanitize.php';
+	
 	class User
 	{
+		private $username;
+		private $password;
 		
-	
-	
+		public __construct($username, $password)
+		{
+			$this->username = Sanitize($username);
+			$this->password = Sanitize($password);
+		}
+		
 	}
 
 ?>
